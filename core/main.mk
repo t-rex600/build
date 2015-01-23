9ee811caa183b01648bc61575db217539858a73b
Chet Kener <Cl3Kener@gmail.com>
2015-01-23 10:32:21 -0500

    Do Not Odex Eng Builds (SQUASHED)
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Do not build test stuff for eng builds
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Remove Debug from Eng
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Eng: Include in user build filter
    
    * Let's make eng builds similar to user builds.  (but with some of the eng build perks as well)
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- core/main.mk
+++ core/main.mk
@@ -344,7 +344,7 @@ endif
 
 ## user/userdebug ##
 
-user_variant := $(filter user userdebug,$(TARGET_BUILD_VARIANT))
+user_variant := $(filter eng user userdebug,$(TARGET_BUILD_VARIANT))
 enable_target_debugging := true
 tags_to_install :=
 ifneq (,$(user_variant))
@@ -386,7 +386,8 @@ endif # !enable_target_debugging
 ## eng ##
 
 ifeq ($(TARGET_BUILD_VARIANT),eng)
-tags_to_install := debug eng
+tags_to_install :=
+WITH_DEXPREOPT := false
 ifneq ($(filter ro.setupwizard.mode=ENABLED, $(call collapse-pairs, $(ADDITIONAL_BUILD_PROPERTIES))),)
   # Don't require the setup wizard on eng builds
   ADDITIONAL_BUILD_PROPERTIES := $(filter-out ro.setupwizard.mode=%,\
@@ -413,7 +414,7 @@ endif
 
 # TODO: this should be eng I think.  Since the sdk is built from the eng
 # variant.
-tags_to_install := debug eng
+tags_to_install := eng
 ADDITIONAL_BUILD_PROPERTIES += xmpp.auto-presence=true
 ADDITIONAL_BUILD_PROPERTIES += ro.config.nocheckin=yes
 else # !sdk
@@ -1036,14 +1037,15 @@ target-java-tests : java-target-tests
 target-native-tests : native-target-tests
 tests : host-tests target-tests
 
-# To catch more build breakage, check build tests modules in eng and userdebug builds.
+# To catch more build breakage, check build tests modules in userdebug builds.
 ifneq ($(ANDROID_NO_TEST_CHECK),true)
 ifneq ($(TARGET_BUILD_PDK),true)
-ifneq ($(filter eng userdebug,$(TARGET_BUILD_VARIANT)),)
+ifneq ($(filter userdebug,$(TARGET_BUILD_VARIANT)),)
 droidcore : target-tests host-tests
 endif
 endif
 endif
+.PHONY: lintall
 
 ifneq (,$(filter samplecode, $(MAKECMDGOALS)))
 .PHONY: samplecode
