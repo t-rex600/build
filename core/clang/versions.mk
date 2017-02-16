ad6bc470e829afd6bc162a6a2c7f944fd7dbae6a
liquid0624 <rjbirnesser@gmail.com>
2017-02-16 18:37:14 -0500

    build: change current clang version to 4.0

--- core/clang/versions.mk
+++ core/clang/versions.mk
@@ -1,4 +1,5 @@
 ## Clang/LLVM release versions.
-LLVM_RELEASE_VERSION := 3.9.1
-LLVM_PREBUILTS_VERSION := 3.9.1
-LLVM_PREBUILTS_BASE := prebuilts/clang/host
+
+LLVM_RELEASE_VERSION := 4.0.0
+LLVM_PREBUILTS_VERSION := 4.0.0
+LLVM_PREBUILTS_BASE ?= prebuilts/clang/host
