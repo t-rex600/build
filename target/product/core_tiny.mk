9326d7197e443e5e4a78541d0eab18c7b23518d8
liquid0624 <rjbirnesser@gmail.com>
2017-02-24 15:57:59 -0500

    build: target dex opt build prop overrides

--- target/product/core_tiny.mk
+++ target/product/core_tiny.mk
@@ -114,10 +114,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Different dexopt types for different package update/install times.
 # On eng builds, make "boot" reasons do pure JIT for faster turnaround.
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    pm.dexopt.first-boot=verify-at-runtime \
-    pm.dexopt.boot=verify-at-runtime
-
+ifeq (eng,$(TARGET_BUILD_VARIANT))
+    PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+        pm.dexopt.first-boot=verify-at-runtime \
+        pm.dexopt.boot=verify-at-runtime
+else
+    PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+        pm.dexopt.first-boot=interpret-only \
+        pm.dexopt.boot=verify-profile
+endif
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     pm.dexopt.install=interpret-only \
     pm.dexopt.bg-dexopt=speed-profile \
