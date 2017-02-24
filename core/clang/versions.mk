724851551d358b2163e4518a942b4a3cf3001146
chad cormier roussel <chadcormierroussel@gmail.com>
2017-02-24 12:08:09 -0500

    Reflect our changes to clang naming

--- core/clang/versions.mk
+++ core/clang/versions.mk
@@ -1,5 +1,8 @@
 ## Clang/LLVM release versions.
-
-LLVM_RELEASE_VERSION := 4.0.0
-LLVM_PREBUILTS_VERSION := 4.0.0
+LLVM_RELEASE_VERSION := 4.0
+LLVM_PREBUILTS_VERSION := 4.0
 LLVM_PREBUILTS_BASE ?= prebuilts/clang/host
+
+# RenderScript-specific tools
+RS_LLVM_PREBUILTS_VERSION := 3.9
+RS_LLVM_PREBUILTS_BASE := prebuilts/clang/host
